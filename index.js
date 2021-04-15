0c386fa0836d2d603fce54b65aae6e2e38306a4f
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2021-04-15 17:52:38 +0100

    test

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 'use strict';
 const hof = require('hof');
 
-const settings = require('./hof.settings');
+const addDynamicSettings = (settings) => {
+  settings.routes = settings.routes.map(route => require(route));
+  settings.root = __dirname;
+  settings.start = false;
 
-if (process.env.REDIS_URL) {
-  settings.redis = process.env.REDIS_URL;
-}
+  if (process.env.REDIS_URL) {
+    settings.redis = process.env.REDIS_URL;
+  }
 
-settings.routes = settings.routes.map(route => require(route));
-settings.root = __dirname;
-settings.start = false;
+  return settings;
+};
 
-module.exports = hof(settings);
+/*
+ During automation test setup, we ping the app with '?automation-test' query to check it is ready before running tests.
+ Put a cookie on this request so it doesn't fail HOF-middleware cookie check.
+*/
+const addAutomationTestCookie = (req, res, next) => {
+  if (req.query['automation-test'] !== undefined) {
+    req.cookies.testCookie = 'test';
+  }
+  return next();
+};
+
+const app = hof(addDynamicSettings(require('./hof.settings')));
+app.use('/start', (req, res, next) => addAutomationTestCookie(req, res, next));
+
+module.exports = app;
